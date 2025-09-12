ddfcd0c0d5ec578a0c98684e233e93da9d142ae8
lucas-ifsp <lucas.ruas@gmail.com>
2025-09-11 22:32:42 -0300

    practical 1

--- basics/src/main/java/practical/Course.java
+++ basics/src/main/java/practical/Course.java
@@ -1,4 +1,84 @@
 package practical;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Course {
+    public enum Room {C102, C104, C105, C106, C107, C209}
+    private static int idCount;
+
+    private int id;
+    private String code;
+    private DayOfWeek dayOfWeek;
+    private String name;
+    private String professor;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Room room;
+
+    public Course(String code, DayOfWeek dayOfWeek, String name, String professor,
+                  LocalTime startTime, LocalTime endTime, Room room) {
+        if (!isValid(code, dayOfWeek, name, professor, startTime, endTime, room)) return;
+        this.id = ++idCount;
+        this.code = code;
+        this.dayOfWeek = dayOfWeek;
+        this.name = name;
+        this.professor = professor;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.room = room;
+    }
+
+    private boolean isValid(String code, DayOfWeek dayOfWeek, String name,
+                            String professor, LocalTime startTime, LocalTime endTime, Room room) {
+        if(code == null || code.isBlank()) return false;
+        if(name == null || name.isBlank()) return false;
+        if(professor == null || professor.isBlank()) return false;
+        if(startTime == null || endTime == null || !startTime.isBefore(endTime)) return false;
+        return room != null;
+    }
+
+    public String getStateAsString() {
+        var formatter = DateTimeFormatter.ofPattern("mm:ss");
+        return String.format("| id = %d | %s (%s) | %s | Start = %s | End = %s | %s | Room = %s |",
+                id, name, code, dayOfWeek, formatter.format(startTime), formatter.format(endTime), professor, room);
+    }
+
+    public int getWeeklyDurationInMinutes() {
+        return (int) Duration.between(startTime, endTime).toMinutes();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
 }
--- basics/src/main/java/practical/Student.java
+++ basics/src/main/java/practical/Student.java
@@ -1,4 +1,60 @@
 package practical;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class Student {
+    private String id;
+    private String name;
+    private LocalDate admissionDate;
+
+    public Student(String id, String name, LocalDate admissionDate) {
+        if (!isValidate(id, name, admissionDate)) return;
+        this.id = id;
+        this.name = name;
+        this.admissionDate = admissionDate;
+    }
+
+    private boolean isValidate(String id, String name, LocalDate admissionDate) {
+        if(id == null || id.isBlank() || ! isValidFormat(id)) return false;
+        if(name == null || name.isBlank()) return false;
+        return admissionDate != null && !admissionDate.isAfter(LocalDate.now());
+    }
+
+    private boolean isValidFormat(String id) {
+        final String lowerCaseId = id.toLowerCase();
+        if(!lowerCaseId.startsWith("sc")) return false;
+        if(id.length() != 9) return false;
+        final char dv = lowerCaseId.charAt(8);
+        if(!Character.isDigit(dv) && dv != 'x') return false;
+        final String middle = lowerCaseId.substring(2, 8);
+        for (char c : middle.toCharArray()) {
+            if(!Character.isDigit(c)) return false;
+        }
+        return true;
+    }
+
+    public int getSemester() {
+        Period period = Period.between(admissionDate, LocalDate.now());
+        final int months =  (int)period.toTotalMonths();
+        return (months / 6) + 1;
+    }
+
+    public String getStateAsString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return String.format("%s | %s | %s", id, name, formatter.format(admissionDate));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getAdmissionDate() {
+        return admissionDate;
+    }
 }
